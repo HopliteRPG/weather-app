5953f94ed2a2e82dca40b9e5bfd534fc7c8fb811
Bill <146594930+HopliteRPG@users.noreply.github.com>
2026-01-24 16:44:40 -0500

    hard coded current weather conditions of new york city to see results

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+async function getLocationWeather() {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/new%20york%20city?unitGroup=us&key=FXHXA475Z37NLG25UUNEYVTVK&contentType=json",
+  );
+  const weatherData = await response.json();
+
+  console.log(weatherData.currentConditions);
+}
+
+getLocationWeather();
