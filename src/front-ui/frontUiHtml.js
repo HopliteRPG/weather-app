cf018cc4d01b3fb50daae90b25461817a59f56cd
Bill <146594930+HopliteRPG@users.noreply.github.com>
2026-01-25 21:56:58 -0500

    created front ui file and weather code file

---
+++ src/front-ui/frontUiHtml.js
@@ -0,0 +1,57 @@
+const body = document.querySelector("body");
+const divCreate = document.createElement("div");
+const buttonCreate = document.createElement("button");
+const pCreate = document.createElement("p");
+const imageCreate = document.createElement("IMG");
+const formCreate = document.createElement("form");
+const inputCreate = document.createElement("input");
+
+function createDivSection(parentDiv, className) {
+  let cloneDivCreate = divCreate.cloneNode(true);
+  cloneDivCreate.classList.add(className);
+  parentDiv.appendChild(cloneDivCreate);
+  return { cloneDivCreate };
+}
+
+function createButton(div, buttonText, buttonClass) {
+  let cloneButtonCreate = buttonCreate.cloneNode(true);
+  cloneButtonCreate.innerText = buttonText;
+  cloneButtonCreate.classList.add(buttonClass);
+  div.appendChild(cloneButtonCreate);
+  return { cloneButtonCreate };
+}
+
+function createP(div, pText, pClass) {
+  let clonePCreate = pCreate.cloneNode(true);
+  clonePCreate.innerText = pText;
+  clonePCreate.classList.add(pClass);
+  div.appendChild(clonePCreate);
+}
+
+function createImage(div, imgClass) {
+  let cloneImageCreate = imageCreate.cloneNode(true);
+  cloneImageCreate.classList.add(imgClass);
+  cloneImageCreate.src = "#";
+  div.appendChild(cloneImageCreate);
+}
+
+function createForm(parentDiv) {
+  let cloneFormCreate = formCreate.cloneNode(true);
+  parentDiv.appendChild(cloneFormCreate);
+  return { cloneFormCreate };
+}
+
+function createInput(div, type, name, inputName) {
+  let cloneInputCreate = inputCreate.cloneNode(true);
+  cloneInputCreate.classList.add(name);
+  div.appendChild(cloneInputCreate);
+  cloneInputCreate.type = type;
+  cloneInputCreate.setAttribute("name", name);
+  cloneInputCreate.classList.add(inputName);
+  return { cloneInputCreate };
+}
+
+function createFrontUiHtml() {
+  let parentDiv = createDivSection(body, "parentDiv");
+  let searchbarForm = createForm(parentDiv.cloneDivCreate);
+}
---
+++ src/weather-code/weatherCode.js
@@ -0,0 +1,8 @@
+async function getLocationWeather() {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/new%20york%20city?unitGroup=us&key=FXHXA475Z37NLG25UUNEYVTVK&contentType=json",
+  );
+  const weatherData = await response.json();
+
+  console.log(weatherData.currentConditions);
+}
