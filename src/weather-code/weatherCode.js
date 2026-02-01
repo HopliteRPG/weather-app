baa46a49c5a16395c3f2ccd7cd33427bd3cf6fb9
Bill <146594930+HopliteRPG@users.noreply.github.com>
2026-01-31 22:49:51 -0500

    createWeatherGrid gets a weeks forcast

--- src/weather-code/weatherCode.js
+++ src/weather-code/weatherCode.js
@@ -1,3 +1,5 @@
+import { createWeatherGrid } from "./weatherCodeHtml";
+
 export { getLocationWeather };
 async function getLocationWeather(location) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=FXHXA475Z37NLG25UUNEYVTVK&contentType=json`;
@@ -9,7 +11,8 @@ async function getLocationWeather(location) {
     }
 
     const weatherData = await response.json();
-    console.log(weatherData.currentConditions);
+    console.log(weatherData);
+    createWeatherGrid(weatherData.days);
   } catch (error) {
     console.error(error);
     alert("Location not found try a different one");
---
+++ src/weather-code/weatherCodeHtml.js
@@ -0,0 +1,60 @@
+export { createWeatherGrid };
+const body = document.querySelector("body");
+const divCreate = document.createElement("div");
+const buttonCreate = document.createElement("button");
+const pCreate = document.createElement("p");
+const imageCreate = document.createElement("IMG");
+const formCreate = document.createElement("form");
+const inputCreate = document.createElement("input");
+
+function createDivSection(parentDiv, className) {
+  let cloneDivCreate = divCreate.cloneNode(true);
+  cloneDivCreate.classList.add(className);
+  parentDiv.appendChild(cloneDivCreate);
+  return { cloneDivCreate };
+}
+
+function createButton(div, buttonText, buttonClass) {
+  let cloneButtonCreate = buttonCreate.cloneNode(true);
+  cloneButtonCreate.innerText = buttonText;
+  cloneButtonCreate.classList.add(buttonClass);
+  div.appendChild(cloneButtonCreate);
+  return { cloneButtonCreate };
+}
+
+function createP(div, pText, pClass) {
+  let clonePCreate = pCreate.cloneNode(true);
+  clonePCreate.innerText = pText;
+  clonePCreate.classList.add(pClass);
+  div.appendChild(clonePCreate);
+}
+
+function createImage(div, imgClass) {
+  let cloneImageCreate = imageCreate.cloneNode(true);
+  cloneImageCreate.classList.add(imgClass);
+  cloneImageCreate.src = "#";
+  div.appendChild(cloneImageCreate);
+}
+
+function createForm(parentDiv) {
+  let cloneFormCreate = formCreate.cloneNode(true);
+  parentDiv.appendChild(cloneFormCreate);
+  return { cloneFormCreate };
+}
+
+function createInput(div, type, name, inputName) {
+  let cloneInputCreate = inputCreate.cloneNode(true);
+  cloneInputCreate.classList.add(name);
+  div.appendChild(cloneInputCreate);
+  cloneInputCreate.type = type;
+  cloneInputCreate.setAttribute("name", name);
+  cloneInputCreate.classList.add(inputName);
+  return { cloneInputCreate };
+}
+
+function createWeatherGrid(weatherWeek) {
+  const weatherWeekForcast = weatherWeek.slice(0, 7);
+  weatherWeekForcast.forEach((day) => {
+    console.log(day);
+  });
+}
