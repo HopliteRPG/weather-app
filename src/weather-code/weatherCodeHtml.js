219d4664e09975e9bc38049ef5bc4edb9ae1ecba
Bill <146594930+HopliteRPG@users.noreply.github.com>
2026-03-07 22:42:11 -0500

    centered dayTemp

--- src/weather-code/weatherCodeHtml.js
+++ src/weather-code/weatherCodeHtml.js
@@ -64,9 +64,21 @@ function createWeatherGrid(weatherWeek) {
   weatherWeekForcast.forEach((day) => {
     let dayDiv = createDivSection(weatherDiv, "dayDiv");
     let dayTemp = createP(dayDiv.cloneDivCreate, day.temp, "dayTemp");
-    let minAndMaxDiv = createDivSection(dayDiv.cloneDivCreate, "minAndMaxDiv");
-    let minTemp = createP(minAndMaxDiv.cloneDivCreate, day.tempmin, "minTemp");
-    let maxTemp = createP(minAndMaxDiv.cloneDivCreate, day.tempmax, "maxTemp");
+    let minAndMaxDivContainer = createDivSection(
+      dayDiv.cloneDivCreate,
+      "minAndMaxDiv",
+    );
+
+    let minTemp = createP(
+      minAndMaxDivContainer.cloneDivCreate,
+      day.tempmin,
+      "minTemp",
+    );
+    let maxTemp = createP(
+      minAndMaxDivContainer.cloneDivCreate,
+      day.tempmax,
+      "maxTemp",
+    );
     let dayWeatherReport = createP(
       dayDiv.cloneDivCreate,
       day.conditions,
